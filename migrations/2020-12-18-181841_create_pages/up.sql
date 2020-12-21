ff327fccfc52b25ced618decae030c907b5e7b6e
spbills <spencebills0@gmail.com>
2020-12-21 16:50:07 -0500

    Added content type for modules and insert for module_types.

--- migrations/2020-12-18-181841_create_pages/up.sql
+++ migrations/2020-12-18-181841_create_pages/up.sql
@@ -10,10 +10,13 @@ CREATE TABLE module_types (
     module_desc varchar(500) NOT NULL
 );
 
+INSERT INTO module_types (title, module_desc) VALUES ('paragraph', 'A paragraph module for general text.');
+
 CREATE TABLE modules (
     module_id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
     module_type_id int NOT NULL,
     page_id int NOT NULL,
+    content TEXT,
     FOREIGN KEY (page_id) REFERENCES pages(page_id) ON DELETE CASCADE,
     FOREIGN KEY (module_type_id) REFERENCES module_types(module_type_id) ON DELETE CASCADE
 );
