14f5707cb2c4c92536b56e71ab7aa8f1db8f6f56
spbills <spencebills0@gmail.com>
2020-12-21 11:18:31 -0500

    Added the implementations for the page model.

--- src/models/models.rs
+++ src/models/models.rs
@@ -1,12 +1,26 @@
 use async_trait::async_trait;
+use diesel::{Connection, MysqlConnection};
+use dotenv::dotenv;
+use std::env;
 
-use super::Page;
+use super::{MutPage, Page};
 
+/// CRUD implementation.
 #[async_trait]
 pub trait Model {
-    async fn create() -> Page;
-    async fn read_one(id: i32) -> Page;
+    async fn create(new_page: &MutPage);
+    async fn read_one(page_id: i32) -> Page;
     async fn read_all() -> Vec<Page>;
-    async fn update(id: u32) -> Page;
-    async fn delete(id: u32) -> Page;
+    async fn update(id: i32, new_page: &MutPage);
+    async fn delete(id: i32);
+}
+
+pub fn establish_database_connection() -> MysqlConnection {
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("Environment variable DATABASE_URL must be set.");
+    MysqlConnection::establish(&db_url).expect(&format!(
+        "Error connecting to database using URL {}",
+        &db_url
+    ))
 }
--- src/models/page.models.rs
+++ src/models/page.models.rs
@@ -1,17 +1,84 @@
-#[derive(Debug, Serialize, Deserialize)]
+use chrono::NaiveDateTime;
+use diesel::{Insertable, Queryable, RunQueryDsl};
+use serde::{Deserialize, Serialize};
+use diesel::prelude::*;
+
+use async_trait::async_trait;
+
+#[path = "../models/models.rs"]
+mod models;
+
+#[path = "../schemas/schema.rs"]
+mod schema;
+
+use schema::pages;
+use models::Model;
+use models::establish_database_connection;
+
+/// The main Rust implementation for the Page model.
+#[derive(Debug, Serialize, Deserialize, Queryable, PartialEq)]
 pub struct Page {
-    pub id: i32,
-    pub created: NaiveDateTime,
-    pub updated: Option<NaiveDateTime>
+    page_id: i32,
+    title: String,
+    time_created: NaiveDateTime,
 }
-#[derive(Insertable, Deserialize, Serialize)]
+/// This acts as both the insertable and update object.
+/// This can be done since pages only really have a `title` column that isn't auto filled.
+#[derive(Insertable, AsChangeset, Deserialize, Serialize)]
 #[table_name = "pages"]
-pub struct NewPage {
-    
+pub struct MutPage {
+    title: String,
 }
 
-#[derive(AsChangeSet, Deserialize, Serialize)]
-#[table_name = "pages"]
-pub struct UpdatePage {
+/// Implementation for Page restricted by models.rs trait.
+/// schema::...::dsl exports all of the columns.
+/// It also exports the table name again. This allows for filtering through the rows of the table.
+/// Every one of these functions exports only what they need out of `dsl`.
+/// Taking all of the columns (for instance whenever using schema::pages::dsl::*) 
+/// is unnecessary and leads to higher RAM usage.
+#[async_trait]
+impl Model for Page {
+    async fn create(new_page: &MutPage) {
+        use schema::pages;
+        let db = establish_database_connection();
 
+        diesel::insert_into(pages::table).values(new_page).execute(&db).unwrap();
+    }
+
+    async fn read_one(id: i32) -> Page {
+        use schema::pages::dsl::pages;
+        use schema::pages::dsl::page_id;
+        
+        let db = establish_database_connection();
+
+        pages.filter(page_id.eq(id)).first::<Page>(&db).unwrap()
+    }
+
+    async fn read_all() -> Vec<Page> {
+        use super::models::Page;
+        use schema::pages;
+
+        let db = establish_database_connection();
+
+        pages::table.load::<Page>(&db).unwrap()
+    }
+
+    async fn update(id: i32, new_page: &MutPage) {
+        use schema::pages::dsl::page_id;
+        use schema::pages::dsl::pages;
+
+        let db = establish_database_connection();
+
+        diesel::update(pages.filter(page_id.eq(id))).set(new_page).execute(&db).unwrap();
+
+    }
+
+    async fn delete(id: i32) {
+        use schema::pages::dsl::page_id;
+        use schema::pages::dsl::pages;
+
+        let db = establish_database_connection();
+
+        diesel::delete(pages.filter(page_id.eq(id))).execute(&db).unwrap();
+    }
 }
