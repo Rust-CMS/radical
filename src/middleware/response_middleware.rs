f0c91c03d25bfcadbb1cc95bb50bc2c08156e407
spbills <spencebills0@gmail.com>
2020-12-22 14:50:04 -0500

    Added response middleware.

---
+++ src/middleware/response_middleware.rs
@@ -0,0 +1,29 @@
+use actix_web::HttpResponse;
+use serde::{Serialize};
+
+use crate::errors_middleware::CustomHttpError;
+
+#[derive(Serialize)]
+pub struct HttpResponseBuilder<T> {
+    code: u16,
+    message: T
+}
+
+impl<Body: Serialize> HttpResponseBuilder<Body> {
+    pub fn new(code: u16, message: &Body) -> Result<HttpResponse, CustomHttpError> {
+        let cm = HttpResponseBuilder {
+            code,
+            message
+        };
+
+        // Eventually, the final arm of this match needs to be a CustomHttpError.
+        let mut m = match code {
+            200 => HttpResponse::Ok(),
+            201 => HttpResponse::Created(),
+            204 => HttpResponse::NoContent(),
+            _ => HttpResponse::BadRequest()
+        };
+
+        Ok(m.body(serde_json::to_string(&cm).unwrap()))
+    }
+}
