2b07127a2aa739ded9ee4b7d00aa7829522afe0b
spbills <spencebills0@gmail.com>
2020-12-30 08:23:06 -0500

    Init for frontend.

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<p>Hello world</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "App",
+	components: {},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
