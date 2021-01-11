144a7a7de35f2058605e14dd250101ea7272524d
spbills <spencebills0@gmail.com>
2021-01-10 19:53:46 -0500

    Added routing to the setup page, along with guards to make sure if setup is still active to redirect to setup.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,27 +1,50 @@
+import Axios from 'axios'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Root from '../pages/Root.vue'
-import Page from "../pages/Page.vue"
-
+import SetupStart from "../pages/setup/SetupStart.vue"
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Root',
-    component: Root
-  },
-  {
-    path: "/pages/:id",
-    name: "Page",
-    component: Page
-  }
+	{
+		path: "/setup",
+		name: "Setup",
+		component: SetupStart
+	},
+	{
+		path: '/',
+		name: 'Root',
+		component: Root,
+		beforeEnter: verifySetup
+	}
 ]
 
+
+var verifySetup = async (to, from, next) => {
+	let allowed_names = ["Setup"];
+
+	if (allowed_names.includes(to.name)) 
+		return next();
+
+	let req = await Axios.get("/config/setup").catch(() => to("Setup"));
+
+	let req_data = req.data.message;
+
+	if (!req_data)
+		return next("Setup");
+
+	if (req_data.config_val === "start")
+		return next("Setup");
+
+	return next();
+}
+
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
 })
+
+router.beforeEach(verifySetup);
 
 export default router
