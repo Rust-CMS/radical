c439b620d95af091646243976d5da93ea2ffdcfc
SpBills <spencebills0@gmail.com>
2021-07-08 20:58:25 +0000

    Basic idea of recursive modules.

--- migrations/2020-12-18-181841_create_pages/up.sql
+++ migrations/2020-12-18-181841_create_pages/up.sql
@@ -19,17 +19,24 @@ INSERT IGNORE INTO module_types (title, module_desc) VALUES ('paragraph', 'A par
 INSERT IGNORE INTO module_types (title, module_desc) VALUES ('header', 'A header module for displaying things in large text.');
 INSERT IGNORE INTO module_types (title, module_desc) VALUES ('image', 'Allows for inserting images into the page.');
 
+CREATE TABLE module_category (
+    id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title varchar(100) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS modules (
     module_id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
     module_type_id int NOT NULL,
     title varchar(100) NOT NULL,
     page_id int NOT NULL,
     content TEXT NOT NULL,
+    category int,
     FOREIGN KEY (page_id) REFERENCES pages(id) ON DELETE CASCADE,
-    FOREIGN KEY (module_type_id) REFERENCES module_types(module_type_id) ON DELETE CASCADE
+    FOREIGN KEY (module_type_id) REFERENCES module_types(module_type_id) ON DELETE CASCADE,
+    FOREIGN KEY (category) REFERENCES module_category(id) ON DELETE CASCADE
 );
 
-INSERT IGNORE INTO modules (module_type_id, title, page_id, content) VALUES (1, "Hello world!", 1, "Hello world!");
+INSERT IGNORE INTO modules (module_type_id, title, page_id, content) VALUES (1, "title", 1, "Hello world!");
 
 CREATE TABLE IF NOT EXISTS web_config (
     config_key VARCHAR(100) PRIMARY KEY NOT NULL,
