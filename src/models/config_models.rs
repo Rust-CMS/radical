3e5edaf1b56f08657e80cf2d57e7ae6c632432fc
SpBills <45398785+SpBills@users.noreply.github.com>
2021-07-28 22:37:26 -0400

    Update config_models.rs

--- src/models/config_models.rs
+++ src/models/config_models.rs
@@ -10,6 +10,6 @@ pub struct LocalConfig {
     pub bind_address: String,
     pub bind_port: u16,
     pub socket_dir: Option<String>,
-    pub sql_name: Option<String>
-    pub max_req: u16
+    pub sql_name: Option<String>,
+    pub max_req: u16,
 }
